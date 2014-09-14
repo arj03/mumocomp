fd26910ac1f30d34d724526c8e68a19bc9089ab2
Anders Rune Jensen <anders@iola.dk>
2014-09-14 21:49:26 +0200

    Add leiningen project file
    This upgrades a lot of libraries including clojure. Not tested very much, so the old files stays for now

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject mumocomp "0.1.0"
+  :description "HTPC management system with a web frontend"
+  :url "https://github.com/arj03/mumocomp"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [de.u-mass/lastfm-java "0.1.2"]
+                 [org/jaudiotagger "2.0.3"]
+                 [org.clojure/data.json "0.2.5"]
+                 [enlive "1.1.5"]
+                 [clj-http "1.0.0"]
+                 [compojure "1.1.9"]
+                 [ring "1.3.1"]
+                 [hiccup "1.0.5"]
+                 ])
