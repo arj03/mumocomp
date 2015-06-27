3045d4c8b604d59b45ae3eac86cad62abd4f5706
Anders Rune Jensen <arj03@protonmail.ch>
2015-06-27 23:29:26 +0200

    Upgrade dependecies

--- project.clj
+++ project.clj
@@ -6,9 +6,9 @@
                  [org/jaudiotagger "2.0.3"]
                  [org.clojure/data.json "0.2.5"]
                  [enlive "1.1.5"]
-                 [clj-http "1.0.0"]
-                 [compojure "1.1.9"]
-                 [ring "1.3.1"]
+                 [clj-http "1.1.2"]
+                 [compojure "1.3.4"]
+                 [ring "1.3.2"]
                  [hiccup "1.0.5"]
                  ]
   :main startup
